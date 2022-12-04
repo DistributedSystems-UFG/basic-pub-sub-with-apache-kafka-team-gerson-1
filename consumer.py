8349716ae73384b6c16b09e6f0faadc500fdd47e
Guilherme Faleiros <guilhermefaleiros2000@gmail.com>
2022-12-03 21:07:36 -0300

    feat: changing to multiple topics and consumers

--- consumer.py
+++ consumer.py
@@ -2,13 +2,15 @@ from kafka import KafkaConsumer
 from const import *
 import sys
 
-consumer = KafkaConsumer(bootstrap_servers=[BROKER_ADDR + ':' + BROKER_PORT])
 try:
-  topic = sys.argv[1]
-except:
+  topics = sys.argv[1:]
+except Exception:
   print ('Usage: python3 consumer <topic_name>')
   exit(1)
-  
-consumer.subscribe([topic])
+
+consumer = KafkaConsumer(bootstrap_servers=[f'{BROKER_ADDR}:{BROKER_PORT}'])
+
+print(f'Starting to listen on {topics}')
+consumer.subscribe(topics)
 for msg in consumer:
     print (msg.value)
--- producer.py
+++ producer.py
@@ -3,15 +3,18 @@ from const import *
 import sys
 
 try:
-    topic = sys.argv[1]
-except:
+    topics = sys.argv[1:]
+except Exception:
     print ('Usage: python3 producer <topic_name>')
     exit(1)
     
-producer = KafkaProducer(bootstrap_servers=[BROKER_ADDR + ':' + BROKER_PORT])
-for i in range(100):
-    msg = 'My ' + str(i) + 'st message for topic ' + topic
-    print ('Sending message: ' + msg)
-    producer.send(topic, value=msg.encode())
+print(BROKER_ADDR)
+producer = KafkaProducer(bootstrap_servers=[f'{BROKER_ADDR}:{BROKER_PORT}'])
+
+for topic in topics:
+    for i in range(100):
+        msg = f'My {str(i)}st message for topic {topic} '
+        print (f'Sending message: {msg}')
+        producer.send(topic, value=msg.encode())
 
 producer.flush()
